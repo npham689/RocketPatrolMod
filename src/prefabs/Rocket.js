3dbb26ff8c6fdf634e69c6ebc1da5d69efadf7fe
Nelson Pham <nelsonpham@Nelsons-MacBook-Pro.local>
2022-04-07 23:17:02 -0700

    Wrote main file and re-modified Rocket speed movement

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -5,7 +5,7 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
         scene.add.existing(this);   // add to existing, displayList, updateList
         this.isFiring = false;      // track rocket's firing status
-        this.moveSpeed = 6;         // pixels per frame
+        this.moveSpeed = 2;         // pixels per frame
         this.sfxRocket = scene.sound.add('sfx_rocket')  // add rocket sfx
     }
 
